faf9f1abc1231c7f7198bf42cf41eff715ed3393
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-03-21 03:50:08 -0300

    feat: final results finished

---
+++ src/components/Success.js
@@ -0,0 +1,13 @@
+export default function Success(){
+    return (
+        <>
+            <article className="result-screen">
+                <span>
+                    <img className="emoji" src="./assets/img/party2.png" alt="emoji party"/>
+                    <h3>Parabéns!</h3>
+                </span>
+                <h2>Você não esqueceu de nenhum flashcard!</h2>
+            </article>
+        </>
+    );
+}
